524a53e7022f5e4c879a6c79f150145067f270e9
DeejayDoubleD <janzendaniel2002@gmail.com>
2023-03-26 15:06:07 +0200

    fixed PEP8 errors

--- Handler/LoginHandler.py
+++ Handler/LoginHandler.py
@@ -1,13 +1,13 @@
 from flask_bcrypt import Bcrypt
-
 from DatabaseConnects.MongoDBConnect import MongoDBConnect
 from pymongo.errors import ServerSelectionTimeoutError
 
+
 class LoginHandler:
 
-    def __init__(self, mongo_db: MongoDBConnect, bcrypt: Bcrypt):
+    def __init__(self, mongo_db: MongoDBConnect, flask_bcrypt: Bcrypt):
         self.mongo_db = mongo_db
-        self.bcrypt = bcrypt
+        self.flask_bcrypt = flask_bcrypt
 
     def login_user(self, username: str, password: str):
         check_username = self.check_username(username)
@@ -35,8 +35,8 @@ class LoginHandler:
         except ServerSelectionTimeoutError as error_message:
             raise error_message
 
-        if check_password is not None and self.bcrypt.check_password_hash(check_password['password'], password) is True:
+        if check_password is not None and self.flask_bcrypt.check_password_hash(check_password['password'], password) \
+                is True:
             return True
         else:
             return False
-
